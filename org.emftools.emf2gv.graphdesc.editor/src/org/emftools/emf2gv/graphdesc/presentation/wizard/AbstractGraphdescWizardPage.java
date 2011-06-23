44fb34b6dcf62ecec27060a5df8c124480a35f8e
Jean-Francois Brazeau <jf.brazeau@yahoo.fr>
2011-06-23 20:30:30 +0000

    License change (BSD -> LGPL + EPL)

--- org.emftools.emf2gv.graphdesc.editor/src/org/emftools/emf2gv/graphdesc/presentation/wizard/AbstractGraphdescWizardPage.java
+++ org.emftools.emf2gv.graphdesc.editor/src/org/emftools/emf2gv/graphdesc/presentation/wizard/AbstractGraphdescWizardPage.java
@@ -1,29 +1,29 @@
 /**
- * Copyright (c) 2010-2011, Jean-Francois Brazeau. All rights reserved.
+ * This file is part of emf2gv : an eclipse plugin that allows to
+ * generate a graphical representation of an EMF model.
+ *
+ * Copyright 2010-2011 Jean-Francois Brazeau
  * 
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions are met:
+ * emf2gv is free software: you can redistribute it and/or modify
+ * it under the terms of either:
  * 
- *  1. Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
+ *      a) the GNU Lesser General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *  or
+ *      b) the Eclipse Public License version 1.0 as published by
+ *       the Eclipse Foundation.
  * 
- *  2. Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
+ * emf2gv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  * 
- *  3. The name of the author may not be used to endorse or promote products
- *     derived from this software without specific prior written permission.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with emf2gv.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIEDWARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * You should have received a copy of the  Eclipse Public License
+ * along with emf2gv.  If not, see <http://www.eclipse.org/legal/epl-v10.html>.
  */
 package org.emftools.emf2gv.graphdesc.presentation.wizard;
 
